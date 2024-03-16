ff0fe393c9a222c33df07f7309c7284f3df13307
xt0032rus <andrey.denikin9@mail.com>
2024-03-16 19:48:34 +0300

    fuxi: fix udfps

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "UdfpsHandler.xiaomi13"
+#define LOG_TAG "UdfpsHandler.xiaomi_sm8550"
 
 #include <android-base/logging.h>
 #include <android-base/properties.h>
