0c0c41775e54415112c3b636d36ed5dfec18a2f7
lolipuru <banstellazz@gmail.com>
2024-06-16 19:03:01 +0700

    fuxi: switch to ext4
    now it's properly handled by kernel
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9648996352
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext system_dlkm product vendor vendor_dlkm odm
 
 BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST))
-$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4))
 $(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # Audio
