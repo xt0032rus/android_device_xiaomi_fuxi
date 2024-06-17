hungphan2001/device_xiaomi_fuxi

fuxi: Keep fod_status enabled during enroll
Change-Id: Ie09efc80c6d4ad06db82c5c693f202f651fd56dd
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

udfps/UdfpsHandler.cpp
@@ -116,7 +116,9 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
         LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         if (result == FINGERPRINT_ACQUIRED_GOOD) {
             setFingerDown(false);
-            setFodStatus(FOD_STATUS_OFF);
+            if (!enrolling) {
+                setFodStatus(FOD_STATUS_OFF);
+            }
         } else if (vendorCode == 20 || vendorCode == 22) {
             /*
              * vendorCode = 20 waiting for fingerprint authentication
@@ -128,13 +130,33 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
 
     void cancel() {
         LOG(INFO) << __func__;
+        enrolling = false;
+
         setFingerDown(false);
+        setFodStatus(FOD_STATUS_OFF);
+    }
+
+    void preEnroll() {
+        LOG(INFO) << __func__;
+        enrolling = true;
+    }
+
+    void enroll() {
+        LOG(INFO) << __func__;
+        enrolling = true;
+    }
+
+    void postEnroll() {
+        LOG(INFO) << __func__;
+        enrolling = false;
+
         setFodStatus(FOD_STATUS_OFF);
     }
 
   private:
     fingerprint_device_t* mDevice;
     android::base::unique_fd touch_fd_;
+    bool enrolling = false;
 
     void setFodStatus(int value) {
         int buf[MAX_BUF_SIZE] = {TOUCH_ID, Touch_Fod_Enable, value};

xt0032rus/android_device_xiaomi_fuxi

udfps/UdfpsHandler.cpp
@@ -240,7 +240,9 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
             req.base.disp_id = MI_DISP_PRIMARY;
             req.local_hbm_value = LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP;
             ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
-            setFodStatus(FOD_STATUS_OFF);
+            if (!enrolling) {
+                setFodStatus(FOD_STATUS_OFF);
+            }
         }
 
         /* vendorCode
@@ -255,6 +257,25 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
 
     void cancel() {
         LOG(DEBUG) << __func__;
+        enrolling = false;
+
+        setFodStatus(FOD_STATUS_OFF);
+    }
+
+    void preEnroll() {
+        LOG(DEBUG) << __func__;
+        enrolling = true;
+    }
+
+    void enroll() {
+        LOG(DEBUG) << __func__;
+        enrolling = true;
+    }
+
+    void postEnroll() {
+        LOG(DEBUG) << __func__;
+        enrolling = false;
+
         setFodStatus(FOD_STATUS_OFF);
     }
 
@@ -262,6 +283,7 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
     fingerprint_device_t* mDevice;
     android::base::unique_fd touch_fd_;
     android::base::unique_fd disp_fd_;
+    bool enrolling = false;
     uint32_t lastPressX, lastPressY;
 
     void setFodStatus(int value) {