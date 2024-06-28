8a3fd9b6d6c0a9b765bbee6dcc89415407f0d200
lolipuru <banstellazz@gmail.com>
2024-06-28 12:36:57 +0700

    fuxi: separated perf related init script
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -449,6 +449,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.recovery.qcom.rc \
     init.recovery.usb.rc \
+    init.perf.fuxi.rc \
     init.service.fuxi.rc \
     init.target.rc \
     init.udfps.fuxi.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -87,6 +87,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.perf.fuxi.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.service.fuxi.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
