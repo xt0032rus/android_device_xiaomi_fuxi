e3e74bcec26794edd601528c8c12b718627456de
xt0032rus <andrey.denikin9@mail.com>
2025-11-22 18:07:00 +0300

    infinity

--- infinity_fuxi.mk
+++ infinity_fuxi.mk
@@ -12,12 +12,14 @@ $(call inherit-product, build/make/target/product/core_64_bit.mk)
 $(call inherit-product, device/xiaomi/fuxi/device.mk)
 
 # Inherit LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := fuxi
-PRODUCT_NAME := lineage_fuxi
+PRODUCT_NAME := infinity_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133C
 PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := Xiaomi/fuxi/fuxi:15/AQ3A.240912.001/OS2.0.213.0.VMCCNXM:user/release-keys
